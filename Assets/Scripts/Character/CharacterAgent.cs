39ea5fc5355876a83ca1b8dd98352abe4fa4c338
Lorenzo Ortiz <jortiz3@rrc.ca>
2023-09-14 14:51:14 -0500

    - Added fields for movement states (movement and chase) and weapons. - Added functions for moving and using weapons. UseWeapon() currently empty.

--- Assets/Scripts/Character/CharacterAgent.cs
+++ Assets/Scripts/Character/CharacterAgent.cs
@@ -8,7 +8,14 @@ public class CharacterAgent : MonoBehaviour
     // This class is in charge of managing the instance of the unit in the level, including tracking its live stats.
 
     [SerializeField] private CharacterData _stats;
+    [SerializeField] private Rigidbody2D _rb;
+    [Header("State Machine Variables")]
+    [SerializeField] private MovementState _movementState;
+    [SerializeField] private MovementState _chaseState;
+    [SerializeField] private List<WeaponInstance> _weapons;
+
     private float _currentHealth;
+    private CharacterAgent _enemyTarget;
 
     public float MaxHealth => _stats.Health;
     public float Armor => _stats.Armor;
@@ -17,6 +24,23 @@ public class CharacterAgent : MonoBehaviour
 
 
     private void Start()
+    {
+        _currentHealth = MaxHealth;
+    }
+
+    private void Update()
+    {
+        //  chase and attack enemy targets, else continue movement
+        if (_enemyTarget) UseWeapons();
+        else MoveCharacter();
+    }
+
+    private void MoveCharacter()
+    {
+        _movementState.MoveAgent(transform, _rb, Speed);
+    }
+
+    private void UseWeapons()
     {
         //
     }
