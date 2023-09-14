d631760000520924931fc2b31b4a06017dc5c86d
Lorenzo Ortiz <jortiz3@rrc.ca>
2023-09-14 15:04:34 -0500

    - Added ProjectileInstance script in new Assets/Scripts/Projectile Scripts folder.

---
+++ Assets/Scripts/Projectile Scripts/ProjectileInstance.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileInstance : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D _rb;
+    private float _damage = 1f;
+    private float _speed = 1f;
+    private float _lifetime = 1f;
+    private float _elapsedLifetime = 0f;
+    public float Damage => _damage;
+    public float Speed => _speed;
+    public float Lifetime => _lifetime;
+
+    public ProjectileInstance(float newDamage, float newSpeed, float newLifetime)
+    {
+        _damage = newDamage;
+        _speed = newSpeed;
+        _lifetime = newLifetime;
+    }
+
+    private void Start()
+    {
+        _rb ??= GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (_elapsedLifetime >= _lifetime) DestroyProjectile();
+        _elapsedLifetime += Time.deltaTime;
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        _rb.MovePosition(transform.position + (transform.up * _speed * Time.fixedDeltaTime));
+    }
+
+    private void DestroyProjectile()
+    {
+        gameObject.SetActive(false);
+    }
+}
--- Assets/Scripts/Weapons/WeaponInstance.cs
+++ Assets/Scripts/Weapons/WeaponInstance.cs
@@ -7,5 +7,8 @@ public abstract class WeaponInstance : MonoBehaviour
     [SerializeField] protected WeaponData _weaponData;
     [SerializeField] protected List<Transform> _projectileSpawnPoints = new List<Transform>();     // positions used to position spawned projectiles
     protected abstract void UseWeapon();
-    protected abstract void FireProjectile();
+    protected virtual void FireProjectile()
+    {
+        //Instantiate()
+    }
 }
