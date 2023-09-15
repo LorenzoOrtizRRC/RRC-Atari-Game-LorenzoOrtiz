e4145d83829055d5b39b34ac9974e060c53873a5
Lorenzo Ortiz <jortiz3@rrc.ca>
2023-09-15 18:50:16 -0500

    - Added minimum and maximum weapon range fields to WeaponData.

--- Assets/Scripts/Weapons/WeaponData.cs
+++ Assets/Scripts/Weapons/WeaponData.cs
@@ -8,8 +8,10 @@ public class WeaponData : ScriptableObject
     [SerializeField] private GameObject _projectileObject;
     [SerializeField] private float _damage;
     [SerializeField] private float _rateOfFire;     // Projectiles per second. Used to calculate weapon cooldown between bullets. Formula for rate of fire: 1 second / bullets per second.
+    [SerializeField] private float _minimumWeaponRange = 0f;
+    [SerializeField] private float _maximumWeaponRange = 5f;
     [SerializeField] private float _projectileSpeed;
-    [SerializeField] private float _projectileLifetime;     // OProjectile lifetime duration in seconds.
+    [SerializeField] private float _projectileLifetime;     // Projectile lifetime duration in seconds.
 
     public GameObject ProjectileObject => _projectileObject;
     public float Damage => _damage;
