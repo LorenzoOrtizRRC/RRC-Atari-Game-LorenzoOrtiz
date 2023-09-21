bf360f2d0938ac0fd5c9259458dbcaa68e38740e
Jose Lorenzo Ortiz <jortiz3@rrc.ca>
2023-09-21 18:59:45 -0500

    - SpawnerData now only has minion prefab and number of minions int for properties.

--- Assets/Scripts/Game Elements/SpawnerData.cs
+++ Assets/Scripts/Game Elements/SpawnerData.cs
@@ -7,8 +7,5 @@ using UnityEngine;
 public class SpawnerData : ScriptableObject
 {
     [SerializeField] private GameObject _minionPrefab;
-    [SerializeField] private bool _spawnInBursts = true;    // if false, spawns continuously
-    [SerializeField] private float _spawnRate = 1f;     // spawns every (_spawnrate) in real seconds. starts after burst spawning finishes.
-    [SerializeField] private float _burstSpawnRate = 1f;    // delay between spawns during burst spawning
-    [SerializeField] private int _burstSpawnCount = 1;  // number of spawns during burst spawning
+    [SerializeField] private int _numberOfMinions;
 }
