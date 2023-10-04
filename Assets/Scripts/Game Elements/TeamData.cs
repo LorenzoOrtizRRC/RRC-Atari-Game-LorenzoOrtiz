2a10d2e27e947e3c6991a6dec34674c97fa161a0
Jose Lorenzo Ortiz <jortiz3@rrc.ca>
2023-10-03 20:17:07 -0500

    - Removed Teams enum in TeamData. Is not needed.

--- Assets/Scripts/Game Elements/TeamData.cs
+++ Assets/Scripts/Game Elements/TeamData.cs
@@ -2,16 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum Team { cat = 0, dog = 1 }
 
 [CreateAssetMenu(fileName = "New Team Data", menuName ="Game/Team Data")]
 public class TeamData : ScriptableObject
 {
-    [SerializeField] private Team _selectedTeam;
     [SerializeField] private string _teamName;
     [SerializeField] private Color _teamColor;
 
-    public Team SelectedTeam => _selectedTeam;
     public string TeamName => _teamName;
     public Color TeamColor => _teamColor;
 }
