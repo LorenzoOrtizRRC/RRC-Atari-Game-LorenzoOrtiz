27ea2595cd262cb3458a2d512ba3f40ad601dc4f
Jose Lorenzo Ortiz <jortiz3@rrc.ca>
2023-10-10 20:08:52 -0500

    - Finished first iteration of MissionObjective script.

--- Assets/Scripts/Game Elements/Mission System/MissionObjective.cs
+++ Assets/Scripts/Game Elements/Mission System/MissionObjective.cs
@@ -3,35 +3,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+enum MissionCompletionCondition {ReachedMaxProgress, LessThanMaxProgress, CurrentProgressIsZero}
+
 [Serializable]
 public class MissionObjective
 {
     public Action<MissionObjective> OnMissionObjectiveUpdated;
 
+    [SerializeField] private string _missionTitle;
+    [SerializeField] private string _missionDescription;
+    [SerializeField] private MissionCompletionCondition _completionCondition = MissionCompletionCondition.ReachedMaxProgress;
     [SerializeField] private List<MissionListener> _missionListeners = new List<MissionListener>();
-    private int _currentProgress = 0;
+
+    private int _currentProgress = 0;       // Initially zero.
     private int _maxProgress = 0;       // When _currentProgress = _maxProgress, this mission is finished.
 
     public int CurrentProgress => _currentProgress;
     public int MaxProgress => _maxProgress;
-    public bool MissionIsComplete => (CurrentProgress / MaxProgress == 1);
 
     public void Initialize()
     {
         foreach (MissionListener missionListener in _missionListeners)
         {
-            missionListener.OnMissionSuccess += AddMissionSuccess;
-            missionListener.OnMissionFailure += AddMissionFailure;
+            missionListener.OnMissionSuccess += TrackMissionSuccess;
+            missionListener.OnMissionFailure += TrackMissionFailure;
         }
     }
 
-    public void AddMissionSuccess()
+    // Returns true if this mission objective is complete.
+    public bool GetCompletionStatus()
+    {
+        if (_completionCondition == MissionCompletionCondition.ReachedMaxProgress)
+        {
+            return CurrentProgress / MaxProgress >= 1;
+        }
+        else if (_completionCondition == MissionCompletionCondition.LessThanMaxProgress)
+        {
+            return CurrentProgress < MaxProgress;
+        }
+        else
+        {
+            return CurrentProgress == 0;
+        }
+    }
+
+    private void TrackMissionSuccess()
     {
         _currentProgress++;
         OnMissionObjectiveUpdated?.Invoke(this);
     }
 
-    public void AddMissionFailure()
+    private void TrackMissionFailure()
     {
         _currentProgress--;
         OnMissionObjectiveUpdated?.Invoke(this);
